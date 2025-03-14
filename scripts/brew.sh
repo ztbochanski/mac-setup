66b2faa8060d30a538fe6de22e1611315ec63895
Zachary Bochanski <33264009+ztbochanski@users.noreply.github.com>
2025-03-14 13:30:10 -0600

    Add brew to path in brew.sh

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -6,6 +6,11 @@ if test ! $(which brew); then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
+# Run these commands in your terminal to add Homebrew to your PATH:
+echo >> /Users/zachbochanski/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/zachbochanski/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 echo "Updating Homebrew..."
 brew update
 
